d8e7ea44b40c450756fecd6ccecf95395fef7d92
Nuthan1443 <nuthanpdd@gmail.com>
2026-05-20 06:34:10 +0530

    fix: correct langchain_text_splitters import

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
